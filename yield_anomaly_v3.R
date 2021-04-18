b5b8f7ff676bd0b295bb27dd2d5896d4c6fcb9f1
pat-k-byrne <pbyrne@ucsb.edu>
2021-04-18 13:37:52 -0700

    Finished generating results. Created a new R markdown document titled "yield_anomaly_mapping" dedicated to this assignment.

--- yield_anomaly_v3.R
+++ yield_anomaly_v3.R
@@ -2,8 +2,8 @@
 #' 
 #' Function to calculate the almond yield difference from the 1980-2003 average (ton/acre)
 #' Function output: a matrix with two columns. Column 1 is input years and column 2 is calculated yield anomaly
-#' @param precip A vector of total precipitation in the month of January (in mm)
-#' @param min_temp A vector of mean minimum daily temperature in the month of February
+#' @param precip A vector containing a timeseries of total precipitation in the month of January (in mm)
+#' @param min_temp A vector containing a timeseries of mean minimum daily temperature in the month of February
 #' @param years.calc A vector of the years for which you would like to calculate yield anomaly (must be the same lenth as precip and min_temp)
 #' @authors
 #' Pat Byrne, Hannah Garcia, and Yani Pohl 
@@ -12,7 +12,7 @@
 #' https://doi.org/10.1016/j.agrformet.2006.10.006
 
 
-yield_anomaly = function(precip, min_temp, years.calc, t1=-0.015, t2=-0.0046, p1=-0.07, p2=0.0043, inter=0.28) {
+yield_anomaly = function(precip, min_temp, t1=-0.015, t2=-0.0046, p1=-0.07, p2=0.0043, inter=0.28) {
   
   calc.anomaly <- t1*min_temp + t2*min_temp^2 + p1*precip + p2*precip^2 + inter # Calculate yield anomaly in year desired based on Lobell equation
   
